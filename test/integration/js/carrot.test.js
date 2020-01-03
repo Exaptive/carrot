efc5ebe583657c1a340c6220df799b7151d701b0
brian.monds <brian.monds@exaptive.com>
2020-01-03 13:41:20 -0600

    Add artifact to job outcomes

--- test/integration/js/carrot.test.js
+++ test/integration/js/carrot.test.js
@@ -11,8 +11,14 @@ function sleep(ms) {
 
 describe('carrot', function() {
   const jobType1 = 'suggestions';
+  const jobType_withArtifacts = 'suggestions_withartifacts';
   const jobInfo1 = {
     jobType: jobType1,
+    args: { individual: false },
+  };
+
+  const jobInfo_withArtifacts = {
+    jobType: jobType_withArtifacts,
     args: { individual: false },
   };
 
@@ -30,6 +36,27 @@ describe('carrot', function() {
       jobInfoJson: jobInfo,
       success: true,
       output,
+    });
+    return ack; // finished (ack so the job isn't repeated)
+  }
+
+  async function suggestionsWithArtifactsHandler(jobInfo) {
+    await sleep(2);
+    workerClient1.reportProgress(jobInfo, 0.25);
+    await sleep(2);
+    workerClient1.reportProgress(jobInfo, 0.5);
+    await sleep(2);
+    const output = { calculation: 'here is a result' };
+    const artifacts = {
+      search: 'https://www.google.com/',
+      repo: 'https://github.com/Exaptive/carrot',
+    };
+
+    const ack = workerClient1.saveResult({
+      jobInfoJson: jobInfo,
+      success: true,
+      output,
+      artifacts,
     });
     return ack; // finished (ack so the job isn't repeated)
   }
@@ -58,6 +85,18 @@ describe('carrot', function() {
     const result = await producerClient1.getResult(jobId);
     expect(result.success).to.equal(true);
     expect(result.output.calculation).to.equal('here is a result');
+  });
+
+  it('should store artifacts in the job outcome', async function() {
+    await workerClient1.register(jobType_withArtifacts, suggestionsWithArtifactsHandler);
+    const jobId = await producerClient1.run(jobInfo_withArtifacts);
+    await sleep(400);
+    const result = await producerClient1.getResult(jobId);
+    const outcome = await producerClient1.getJobOutcome(jobId);
+    expect(result.success).to.equal(true);
+    expect(result.output.calculation).to.equal('here is a result');
+    expect(outcome.artifacts.search).to.equal('https://www.google.com/');
+    expect(outcome.artifacts.repo).to.equal('https://github.com/Exaptive/carrot');
   });
 
   it('should send progress and done events', async function() {
