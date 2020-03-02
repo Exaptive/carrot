8248b252868c010259b9159281d681e1373a982e
brian.monds <brian.monds@exaptive.com>
2020-03-02 08:00:41 -0600

    Long running callbacks block heartbeats

--- carrot/carrot.py
+++ carrot/carrot.py
@@ -13,6 +13,8 @@ import json
 
 from bson.objectid import ObjectId
 import bson
+import threading
+import Queue
 
 class Worker:
     def __init__(self, config):
@@ -43,6 +45,10 @@ class Worker:
             properties=pika.BasicProperties(delivery_mode=2)) # persistent message
 
     def reportProgress(self, jobInfo, progress, data = {}):
+        p = partial(self._reportProgress, jobInfo, progress, data)
+        self.broker.add_callback_threadsafe(p)
+
+    def _reportProgress(self, jobInfo, progress, data = {}):
         print(' [x] Reporting progress')
         eventInfo = {
             'eventJobType': jobInfo['jobType'],
@@ -57,6 +63,10 @@ class Worker:
             properties=pika.BasicProperties(delivery_mode=2)) # persistent message
 
     def saveResult(self, jobInfo, success, output, output_ttl = None, artifacts = {}):
+        p = partial(self._saveResult, jobInfo, success, output, output_ttl, artifacts)
+        self.broker.add_callback_threadsafe(p)
+
+    def _saveResult(self, jobInfo, success, output, output_ttl = None, artifacts = {}):
         # record result in redis
         _id = ObjectId(jobInfo['jobId'])
         resultKey = self._resultKey(_id)
@@ -96,13 +106,33 @@ class Worker:
         self.broker.close()
         # the redis.Redis connection pool (self.redis) handles disconnect
 
+    def _threaded_callback(self, workerCallback, jobInfo, queue):
+        ack = workerCallback(jobInfo)
+        queue.put(ack)
+
     def _callback(self, workerCallback, ch, method, properties, body):
         jobInfo = json.loads(body)
         # make sure event queue exists for the worker
         # it is more efficient to do this once per job than in reportProgress/reportDone
         eventReturnQueue = jobInfo['eventReturnQueue']
         self.channel.queue_declare(eventReturnQueue, durable=True)
-        ack = workerCallback(jobInfo)
+
+        # Run the callback using a thread so the main thread can handle heartbeats
+        queue = Queue.Queue()
+        t = threading.Thread(
+            target=self._threaded_callback,
+            args=(workerCallback, jobInfo, queue)
+        )
+        t.start()
+
+        while t.is_alive():
+            # Give pika time to process other events
+            self.broker.process_data_events()
+
+            # Pause the loop, but in a safer way for pika and broker communication
+            self.broker.sleep(15)
+        ack = queue.get()
+
         if (ack):
             self.channel.basic_ack(delivery_tag=method.delivery_tag)
         self.reportDone(jobInfo)
